7dda18c4905b269d9cf4db1e93462ab55789c096
teruhito <ataeteruhito@gmail.com>
2021-05-13 16:34:40 +0900

    feat: HomeView

---
+++ SwiftUISample_8GitHub/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SwiftUISample_8GitHub
+//
+//  Created by 若江照仁 on 2021/05/13.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
