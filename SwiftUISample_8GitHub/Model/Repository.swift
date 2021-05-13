b18eab0e608898d50b95f0f0bd35f243c1d3349e
teruhito <ataeteruhito@gmail.com>
2021-05-13 17:18:31 +0900

    feat: add Repository

---
+++ SwiftUISample_8GitHub/Model/Repository.swift
@@ -0,0 +1,12 @@
+//
+//  Repository.swift
+//  SwiftUISample_8GitHub
+//
+//  Created by 若江照仁 on 2021/05/13.
+//
+
+import Foundation
+
+struct Repository: Decodable, Hashable, Identifiable {
+    let id: Int
+}
