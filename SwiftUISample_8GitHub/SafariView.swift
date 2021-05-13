bfa82d57ae896f89f8ce933fc09b6ddda3f45d2e
teruhito <ataeteruhito@gmail.com>
2021-05-13 17:14:59 +0900

    feat: add SafariView

---
+++ SwiftUISample_8GitHub/SafariView.swift
@@ -0,0 +1,19 @@
+//
+//  SafariView.swift
+//  SwiftUISample_8GitHub
+//
+//  Created by 若江照仁 on 2021/05/13.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = SFSafariViewController
+    var url: URL
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
