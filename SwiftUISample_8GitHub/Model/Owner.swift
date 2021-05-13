c3a5db7065212f4c50647d1f14b155eeefefc557
teruhito <ataeteruhito@gmail.com>
2021-05-13 17:19:36 +0900

    feat: add Owner

---
+++ SwiftUISample_8GitHub/Model/Owner.swift
@@ -0,0 +1,13 @@
+//
+//  Owner.swift
+//  SwiftUISample_8GitHub
+//
+//  Created by 若江照仁 on 2021/05/13.
+//
+
+import Foundation
+
+struct Owner: Decodable, Hashable, Identifiable {
+    let id: Int
+    let avatarUrl: String
+}
